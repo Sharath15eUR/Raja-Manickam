7640b563f9b898adf84c0b3d71d3f1496aac002f
Raaj-Manick <vraajmanik@gmail.com>
2025-02-03 11:26:31 +0530

    changes

--- Linux/Module_2/Q4/high_memory_process.sh
+++ Linux/Module_2/Q4/high_memory_process.sh
@@ -1,14 +1,21 @@
 #!/bin/bash
 
-PID=$(ps -eo pid,%mem --sort=-%mem | awk 'NR==2 {print $1}') #fetching the process with high memory usage 
+# Get the PID, memory usage, and name of the process with the highest memory usage
+PROCESS_INFO=$(ps -eo pid,%mem,comm --sort=-%mem | awk 'NR==2 {print $1, $2, $3}')
+PID=$(echo $PROCESS_INFO | awk '{print $1}')
+MEMORY=$(echo $PROCESS_INFO | awk '{print $2}')
+PROCESS_NAME=$(echo $PROCESS_INFO | awk '{print $3}')
 
-if [ -n "$PID" ]; then
-    kill -9 $PID
-    if [ $? -eq 0 ]; then #checks if exit status of last command is 0(successful)
-        echo "process $PID has been terminated"
-    else
-        echo "Process $PID not terminated"
-    fi
-else
-    echo "process not found "
-fi
+# Printing the  formation to the terminal and save it to a file 
+{
+  echo "Process with highest memory usage:"
+  echo "PID: $PID"
+  echo "Memory Usage: $MEMORY%"
+  echo "Process Name: $PROCESS_NAME"
+} | tee process_info.txt
+
+# Terminate the process
+kill -9 $PID
+
+# Informing the user that the process has been terminated
+echo "Process $PID ($PROCESS_NAME) has been terminated." | tee -a process_info.txt
